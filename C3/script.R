a8339e0f652b65734b4b8be87bc182cd71982b3c
cecilesauder <cecile.sauder@gmail.com>
2016-09-12 23:57:59 +0200

    premier widget

---
+++ C3/script.R
@@ -0,0 +1,31 @@
+if (!require("jsonlite"))
+  install.packages("jsonlite")
+
+library(jsonlite)
+toJSON(list(data = 75))
+
+install.packages("htmlwidgets")
+install.packages("devtools")
+
+library("htmlwidgets")
+library("devtools")
+
+# create package using devtools
+devtools::create("C3")  
+
+# navigate to package dir
+setwd("C3")       
+
+# create widget scaffolding
+scaffoldWidget("C3Gauge", edit = FALSE) 
+
+# install package so we can test it
+install()     
+
+library(C3)
+C3Gauge("hello, world")
+
+
+devtools::install()                                      
+library(C3)
+C3Gauge(20)
